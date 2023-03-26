62d5ecafd79f9b806cdf7c62b79c5287395b3cce
dipan <mandal.common@gmail.com>
2023-03-27 00:45:02 +0530

    feat: password field eyelash

--- src/app/modules/public/components/login-v1/login-v1.component.ts
+++ src/app/modules/public/components/login-v1/login-v1.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faL } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login-v1',
   templateUrl: './login-v1.component.html',
-  styleUrls: ['./login-v1.component.css']
+  styleUrls: ['./login-v1.component.css'],
 })
 export class LoginV1Component {
-
+  showPassword: boolean = false;
 }
