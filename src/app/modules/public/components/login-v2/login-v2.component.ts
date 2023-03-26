683578fdd4d5dd4fb8dc5f5e5c59297c16184f0d
dipan <mandal.common@gmail.com>
2023-03-26 21:00:25 +0530

    refactor: login v2 working fine

--- src/app/modules/public/components/login-v2/login-v2.component.ts
+++ src/app/modules/public/components/login-v2/login-v2.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-v2',
   templateUrl: './login-v2.component.html',
-  styleUrls: ['./login-v2.component.css']
+  styleUrls: ['./login-v2.component.css'],
 })
 export class LoginV2Component {
-
+  selectedTab: string = 'login';
 }
